49fe5ab9379e94e040255725a36dd588516c2eb2
Angel <Angel@192.168.1.41>
2022-10-18 22:34:16 +0530

    first commit

--- TNS_B28/src/assignment/Details.java
+++ TNS_B28/src/assignment/Details.java
@@ -1,9 +1,25 @@
 package assignment;
+import java.util.Scanner;
 
 public class Details {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the details:");
+		String name=sc.nextLine();
+		int age=sc.nextInt();
+		int s1_mark=sc.nextInt();
+		int s2_mark=sc.nextInt();
+		int s3_mark=sc.nextInt();
+		int total_marks=s1_mark+s2_mark+s3_mark;
+		float avg=total_marks/3;
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Subject1 marks: "+s1_mark);
+		System.out.println("Subject2 marks: "+s2_mark);
+		System.out.println("Subject3 marks: "+s3_mark);
+		System.out.println("Total marks: "+total_marks);
+		System.out.println("Average: "+avg);
 
 	}
 
