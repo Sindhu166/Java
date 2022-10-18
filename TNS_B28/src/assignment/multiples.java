ff104d089d4a260da4a2d3fba5d0e2b8cfc6e037
Angel <Angel@192.168.1.41>
2022-10-18 22:15:04 +0530

    first commit

---
+++ TNS_B28/src/assignment/multiples.java
@@ -0,0 +1,19 @@
+package assignment;
+import java.util.Scanner;
+
+public class multiples {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n = sc.nextInt();
+		if(n%3==0) {
+			System.out.println(n+" is a multiples of 3");
+		}
+		else {
+			System.out.println(n+" is not a multiples of 3");
+		}
+
+	}
+
+}
