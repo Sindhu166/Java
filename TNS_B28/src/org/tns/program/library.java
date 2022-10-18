3d463049c265f42f67bbfd333726a93a19b678b6
Angel <Angel@ASHWINISIVAKUMAR>
2022-10-18 13:56:37 +0530

    second commit

--- TNS_B28/src/org/tns/program/library.java
+++ TNS_B28/src/org/tns/program/library.java
@@ -1,10 +1,21 @@
 package org.tns.program;
+import java.util.Scanner;
 
 public class library {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter No of days :");
+		int days = sc.nextInt();
+		if(days<=5) {
+			System.out.println("The fine is "+days*1);
+		}
+		else if(days>5 && days<=10) {
+			System.out.println("The fine is "+days*5);
+		}
+		else {
+			System.out.println("The fine is "+days*10);
+		}
 	}
 
 }
